0a5eae6fc75375d46418363b554b68335bd19e20
sncfsamy <samy.lamiri@sncf.fr>
2022-12-12 09:54:22 +0100

    Adaptat with useReducer done

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,54 +1,71 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import { Link } from 'react-router-dom'
 import PeopleCard from '../peopleCard'
 
 function Home() {
-  const [data, setData] = useState()
-  const [error, setError] = useState()
-  const [isLoading, setIsLoading] = useState(false)
-  const [isNewRequest, setIsNewRequest] = useState(false)
-
+  const reducer = (state,action) => {
+    switch(action.type) {
+      case 'setData':
+        return {...state, data: action.data};
+      case 'setError':
+        return {...state, error: action.data};
+      case 'setIsLoading':
+        return {...state, isLoading: action.data};
+      case 'setIsNewRequest':
+        return {...state, isNewRequest: action.data}
+      default:
+        throw new Error();
+    }
+  };
+  const [state,dispatch] = useReducer(reducer, {
+    data:null,
+    error: "",
+    isLoading: false,
+    isNewRequest: false
+  });
   useEffect(() => {
-    setIsLoading(true)
+    dispatch({type: 'setIsLoading', data: true});
     fetch('https://swapi.dev/api/people')
       .then((response) => {
-        setIsLoading(false)
+        dispatch({type: 'setIsLoading', data: false});
         return response.json()
       })
       .then((data) => {
         if (data.results.length > 0) {
-          return setData(data)
+          return dispatch({type: 'setData', data: data});
         } else {
-          setIsLoading(false)
-          return setError('Pas de données trouvées')
+          dispatch({type: 'setIsLoading', data: false});
+          return dispatch({type: 'setError', data: 'Pas de données trouvées'});
         }
       })
       .catch(() => {
-        setIsLoading(false)
-        setError('Une erreur est survenu')
+        dispatch({type: 'setIsLoading', data: false});
+        return dispatch({type: 'setError', data: 'Une erreur est survenu'});
       })
-  }, [isNewRequest])
+  }, [state.isNewRequest])
   console.log('render')
+  console.log(state);
   return (
     <div className="home">
       <div>
-        {isLoading && <div>Loading</div>}
-        {data && (
+        {state.isLoading && <div>Loading</div>}
+        {state.data && (
           <div>
-            {data.results.map((people, index) => (
+            {state.data.results.map((people, index) => (
               <Link key={index} to={`/people/${index + 1}`}>
                 <PeopleCard {...people} />
               </Link>
             ))}
             <button
               className="mt-4 p-3 text-white bg-black"
-              onClick={() => setIsNewRequest(!isNewRequest)}
+              onClick={() => 
+                dispatch({type: 'setIsNewRequest', data: !state.isNewRequest})}
             >
               Relancer la requête
             </button>
           </div>
         )}
-        {error && <div className="error">{error}</div>}
+        {state.error && <div className="error">{state.error}</div>}
       </div>
     </div>
   )
